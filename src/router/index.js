3dff1ef82d1e49f7f9b8f38f184d8fa8aa080e7f
Karol Kozakowski <me@karolkozakowski.io>
2019-12-26 15:02:08 +0100

    Stop using deleted views

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '../views/Home.vue';
-import App from '../views/App.vue';
-
 import Breakfast from '../views/Breakfast.vue';
 import Snack from '../views/meals/Snack.vue';
 
@@ -16,30 +13,9 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/login',
     name: 'login',
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
-  },
-  {
-    path: '/app',
-    name: 'app',
-    component: App,
-    meta: {
-      requiresAuth: true
-    }
   },
   {
     path: '/breakfast',
